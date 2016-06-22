3b2124a31c3039462a5911151cf8da1f597802d1
Mark <mcmarco2@gmail.com2>
2016-06-21 22:48:25 -0500

    serving static files

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var express = require('express');
 
 var app = express();
 
+app.use('/static', express.static('src/public'));
+
+app.set('view engine', 'jade');
+app.set('views',__dirname + '/src/templates');
+
 app.get('/', function(req, res){
-  res.send('Challenge')
+  res.render('index');
 });
 
 app.listen(3000, function(){
-  console.log('The frontend server is running on port 3000')
+  console.log('The frontend server is running on port 3000');
 });
