a595ed64855396e78db878d1c3b8c8f28305efa3
Mark <mcmarco2@gmail.com2>
2016-06-21 20:56:11 -0500

    installing nodemon

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ var express = require('express');
 
 var app = express();
 
-app.listen(3000);
+app.get('/', function(req, res){
+  res.send('Challenge')
+});
+
+app.listen(3000, function(){
+  console.log('The frontend server is running on port 3000')
+});
