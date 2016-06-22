95099fb3068662eac5d2171d121957df4a974d6c
Mark <mcmarco2@gmail.com2>
2016-06-22 06:57:59 -0500

    html5 localstorage

--- src/public/js/controllers/item.controller.js
+++ src/public/js/controllers/item.controller.js
@@ -2,9 +2,22 @@
 
 angular.module('foodApp').controller('ItemController', function($scope, DataService, $stateParams){
   var id = $stateParams.id;
+  var reportLS = localStorage.getItem(id);
+  if (typeof(Storage) !== "undefined") {
+    if (reportLS !== null){
+      reportLS = JSON.parse(reportLS);
+      $scope.report = reportLS;
+      console.log('Retrieved from local storage')
+    }else{
+      DataService.getReport(id).then(function (res) {
+        $scope.report = res;
+        localStorage.setItem(id, JSON.stringify(res));
+        console.log('Local storage created')
+      });
+    }
+  }else{
+    console.log('your browser does not support HTML5 local storage')
+  }
 
-  DataService.getReport(id).then(function (res) {
-    $scope.report = res;
-  });
 
 });
--- src/public/js/controllers/search.controller.js
+++ src/public/js/controllers/search.controller.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('foodApp').controller('SearchController', function($scope, DataService){
-  DataService.getList().then(function (res) {
-    $scope.foodList = res;
-  });
+  var foodListLS = localStorage.getItem("foodList");
+  if (typeof(Storage) !== "undefined") {
+    if (foodListLS !== null){
+      foodListLS = JSON.parse(foodListLS);
+      $scope.foodList = foodListLS;
+      console.log('Retrieved from local storage')
+    }else{
+      DataService.getList().then(function (res) {
+        $scope.foodList = res;
+        localStorage.setItem("foodList", JSON.stringify(res));
+        console.log('Local storage created')
+      });
+    }
+  }else{
+    console.log('your browser does not support HTML5 local storage')
+  }
+  
 
 });
